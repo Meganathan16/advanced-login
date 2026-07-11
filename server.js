856613a28dc952d0edd443dd5828c8469a3ed87d
Meganathan16 <116368899+Meganathan16@users.noreply.github.com>
2026-07-11 17:20:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -365,9 +365,6 @@ app.get("/", (req, res) => {
 // ==============================
 
 
-
-const PORT = process.env.PORT || 5000;
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
